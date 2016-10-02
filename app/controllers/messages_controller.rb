6002cf59772e240fc6c8cc58d3db64660f6233a3
Alvaro Agüero V <aguero.vera.alvaro@gmail.com>
2016-10-02 16:35:09 -0300

    AnoniBook v1.0.2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,33 @@
 class MessagesController < ApplicationController
+	
+	skip_before_filter :verify_authenticity_token
+
 	def index
 		@messages=Message.all
+		@messages=Message.order('created_at DESC')
+	end
+
+	def show
+		@messages=Message.find(params[:id])
+	end
+
+	def new
+		@messages=Message.new
+	end
+
+	def create
+		@messages=Message.new(message_params)
+		
+		if @messages.save
+			redirect_to "/messages"
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:title,:body,:number)
 	end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+	validates :title, presence: true, length: { maximum: 30 }
+	validates :body, presence: true
+	validates :number, presence: true, numericality: { only_integer: true }
+	validates :body, presence: true, length: { minimum: 20 }
 end
